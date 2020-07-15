cfea80296187672e31f4683534c30ac5f4a3cc29
Khan Kibria <khan.kibria@gmail.com>
2020-07-15 02:09:02 -0700

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
-	import Working from "./Working.svelte";
-	   let colors = ['blue', 'purple', 'red'];
-  </script>
-  
-  <h1>Pure svelte spinners</h1>
-  <h2>Framework agnostic</h2>
-  <h3>Uses css animation and css variables</h3>
-  <Working {colors}/>
+  import Working from "./Working.svelte";
+  import Barloader from "./Barloader.svelte";
+
+  let colors = ["blue", "purple", "red"];
+</script>
+
+<h1>Pure svelte spinners</h1>
+<h2>Framework agnostic</h2>
+<h3>Uses css animation and css variables</h3>
+<Working {colors} />
+<p />
+<Barloader />
---
+++ src/Barloader.svelte
@@ -0,0 +1,80 @@
+<script>
+  import chroma from "chroma-js";
+  export let size = "5em";
+  export let color = "#FF3E00";
+
+  let bgcolor = chroma(color).alpha(0.2);
+
+  let css_var = `
+--size: ${size};
+--color: ${color};
+--bgcolor: ${bgcolor};
+`;
+</script>
+
+<style>
+  @keyframes long {
+    0% {
+      left: -35%;
+      right: 100%;
+    }
+    60% {
+      left: 100%;
+      right: -90%;
+    }
+    100% {
+      left: 100%;
+      right: -90%;
+    }
+  }
+
+  @keyframes short {
+    0% {
+      left: -200%;
+      right: 100%;
+    }
+    60% {
+      left: 107%;
+      right: -8%;
+    }
+    100% {
+      left: 107%;
+      right: -8%;
+    }
+  }
+
+  .wrapper {
+    height: calc(var(--size) / 15);
+    width: calc(var(--size) * 2);
+    background-color: var(--bgcolor);
+    position: relative;
+    overflow: hidden;
+    background-clip: padding-box;
+  }
+
+  .lines {
+    height: calc(var(--size) / 15);
+    background-color: var(--color);
+    position: absolute;
+    overflow: hidden;
+    background-clip: padding-box;
+    display: block;
+    border-radius: 2px;
+    will-change: left, right;
+    animation-fill-mode: forwards;
+  }
+
+  .long {
+    animation: long 2.1s ease-in-out 0s infinite normal both running;
+  }
+  .short {
+    animation: short 2.1s ease-in-out 0s infinite normal both running;
+    opacity: 0.65;
+  }
+</style>
+
+
+<div class="wrapper" style={css_var}>
+  <div class="lines long" />
+  <div class="lines short" />
+</div>
